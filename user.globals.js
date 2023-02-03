c90e4494ae6f771cd3720a43c14ad2222d129bc1
Milko Škofič <milko@skofic.io>
2023-02-03 18:41:34 +0100

    Added and corrected test terms.

--- user.globals.js
+++ user.globals.js
@@ -17,6 +17,6 @@
  */
 
 const fs = require('fs');
-const user = JSON.parse(fs.readFileSync('dbCredentials.skobo.json'))
+const user = JSON.parse(fs.readFileSync('dbCredentials.localhost.json'))
 
 module.exports = { user }
